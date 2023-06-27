b3e16ec0e8ecef4cb5455b1c18e240f782bdbc06
stevieg-o1 <steven.karanja@student.moringaschool.com>
2023-06-27 15:26:20 +0300

    completed

---
+++ app/serializers/director_serializer.rb
@@ -0,0 +1,7 @@
+class DirectorSerializer < ActiveModel::Serializer
+
+    attributes :id, :name, :birthplace, :female_director
+  
+    has_many :movies, serializer: DirectorMovieSerializer
+  
+  end
